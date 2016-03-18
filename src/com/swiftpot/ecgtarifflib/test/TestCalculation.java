2f8d2ea9362faeb043f2f2519d77f2ed98ef4de3
AcePRbk[Rodney] <rodney@smsgh.com>
2016-03-18 13:48:46 +0000

    First Commit with working Calculation implementation

---
+++ src/com/swiftpot/ecgtarifflib/test/TestCalculation.java
@@ -0,0 +1,13 @@
+package com.swiftpot.ecgtarifflib.test;
+
+/**
+ * @author Ace Programmer Rbk
+ *         <Rodney Kwabena Boachie at rbk.unlimited@gmail.com> on
+ *         18-Mar-16
+ */
+public class TestCalculation {
+
+    public static void main(String args[]){
+
+    }
+}
